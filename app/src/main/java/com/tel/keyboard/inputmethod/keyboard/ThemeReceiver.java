9b077000448163cf9016e60fd78a8f613b5e7612
v1nc <github@reckendrees.systems>
2021-02-15 22:52:19 +0100

    implement keyboard color changing

--- app/src/main/java/com/tel/keyboard/inputmethod/keyboard/ThemeReceiver.java
+++ app/src/main/java/com/tel/keyboard/inputmethod/keyboard/ThemeReceiver.java
@@ -3,11 +3,25 @@ package com.tel.keyboard.inputmethod.keyboard;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.widget.Toast;
+
+import com.tel.keyboard.inputmethod.compat.PreferenceManagerCompat;
+import com.tel.keyboard.inputmethod.latin.settings.Settings;
 
 public class ThemeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Theme Receiver activated!", Toast.LENGTH_LONG).show();
+        String colorString = intent.getStringExtra("com.tel.keyboard.inputmethod.THEME");
+        try{
+            int color = Color.parseColor(colorString);
+            SharedPreferences mPrefs = PreferenceManagerCompat.getDeviceSharedPreferences(context);
+            SharedPreferences.Editor editor = mPrefs.edit();
+            editor.putInt(Settings.PREF_KEYBOARD_COLOR,color);
+            editor.commit();
+        }catch(IllegalArgumentException e){
+            Toast.makeText(context, "[TEL:Keyboard]: Can not parse color!", Toast.LENGTH_LONG).show();
+        }
     }
 }
